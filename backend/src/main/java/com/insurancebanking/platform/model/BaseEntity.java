71db3e6b4d311bfe94866f63774b723e7bd67f17
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-19 21:23:39 +0200

    Initialize BaseEntity model

---
+++ backend/src/main/java/com/insurancebanking/platform/model/BaseEntity.java
@@ -0,0 +1,35 @@
+// backend/src/main/java/com/insurancebanking/app/model/BaseEntity.java
+
+package com.insurancebanking.platform.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
