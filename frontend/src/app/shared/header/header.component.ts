832b1acb92371dbadca426fc2321adffb4dda66a
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-21 18:35:34 +0200

    frontend: Initialize header component

---
+++ frontend/src/app/shared/header/header.component.ts
@@ -0,0 +1,13 @@
+// src/app/shared/header/header.component.ts
+
+import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  constructor(public authService: AuthService) {}
+}
