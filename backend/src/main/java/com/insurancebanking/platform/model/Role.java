d8927b38824a72fb92327ea95091795f2adfbec4
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-21 17:08:23 +0200

    backend: model Role. Add equals and hashcode methods. Remove builder default annonation from users set for hibernate injection.

--- backend/src/main/java/com/insurancebanking/platform/model/Role.java
+++ backend/src/main/java/com/insurancebanking/platform/model/Role.java
@@ -35,6 +35,18 @@ public class Role {
     private String name;
 
     @ManyToMany(mappedBy = "roles")
-    @Builder.Default
     private Set<User> users = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return id != null && id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
