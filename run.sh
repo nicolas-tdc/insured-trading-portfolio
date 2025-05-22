a1037e4652ad49545ab140645c57f60989075dd7
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-22 16:23:28 +0200

    global: Improve run script logs. Check url as function. Add debug mode to run_service function

--- run.sh
+++ run.sh
@@ -21,12 +21,10 @@ if ! command -v curl &> /dev/null; then
 fi
 
 # Set the mode based on the first argument
-mode=""
+MODE=""
 if [ "$1" == "dev" ]; then
-  mode="dev"
-  echo "Starting in DEV mode (hot-reload enabled)"
-else
-  echo "Starting in PROD mode"
+  MODE="dev"
+  echo "Development mode enabled"
 fi
 
 # Load global env if exists
@@ -38,59 +36,82 @@ fi
 # Kill running services
 echo "Killing existing services..."
 if [ -f "kill.sh" ]; then
-  bash ./kill.sh $mode > /dev/null 2>&1
+  bash ./kill.sh "$MODE" > /dev/null 2>&1
 else
   echo "kill.sh not found."
 fi
 
 run_service() {
     local service_dir=$1
+    local debug=$2
 
     local run_script="run.sh"
 
     if [ -f "$run_script" ]; then
         cd "$service_dir"
-        bash "$run_script" $MODE > /dev/null 2>&1 &
+
+        if [ "$debug" == "debug" ]; then
+          echo "Debug enabled"
+          bash "$run_script" "$MODE"
+        else
+          echo "Debug disabled"
+          bash "$run_script" "$MODE" > /dev/null 2>&1 &
+        fi
+
         cd ..
     else
         echo "$run_script not found in $service_dir."
     fi
 }
 
+check_url() {
+  local url=$1
+  local sleep_time=$2
+  local max_tries=$3
+
+  try_count=0
+  max_tries=10
+  while true; do
+    status_code=$(curl -o /dev/null -s -w "%{http_code}" "$url")
+
+    if [[ \
+      "$status_code" == "200" || \
+      "$status_code" == "302" || \
+      "$status_code" == "401" \
+      ]]; then
+      echo -e "🚀 \e[32mAccess available at\e[0m $url"
+      break
+    else
+      try_count=$((try_count + 1))
+      echo "Testing $url [$try_count/$max_tries]"
+
+      if [ $try_count -ge $max_tries ]; then
+        echo "❌ Application is not accessible after $max_tries tries. Exiting..."
+        exit 1
+      fi
+
+      echo "Retrying in 10 seconds..."
+      sleep 10
+    fi
+  done
+}
+
 # Start Database
-echo "Starting Database..."
+echo -e "\e[33mStarting\e[0m Database..."
 run_service "database"
 
 # Start Backend
-echo "Starting Backend..."
+echo -e "\e[33mStarting\e[0m Backend..."
 run_service "backend"
 
+# Check if Backend service is accessible
+echo -e "\e[33mWaiting\e[0m for backend..."
+check_url "http://localhost:8080" 10 10
+
 # Start Frontend
-echo "Starting Frontend..."
+echo -e "\e[33mStarting\e[0m Frontend..."
 run_service "frontend"
 
-# Check if the application is accessible
-url="http://localhost:8080"
-
-echo "Waiting for $url to become accessible..."
-try_count=0
-max_tries=10
-while true; do
-  status_code=$(curl -o /dev/null -s -w "%{http_code}" "$url")
-
-  if [[ "$status_code" == "200" || "$status_code" == "302" || "$status_code" == "401" ]]; then
-    echo "🚀 Access the application at: $url"
-    exit 0
-  else
-    try_count=$((try_count + 1))
-    echo "Testing $url [$try_count/$max_tries]"
-
-    if [ $try_count -ge $max_tries ]; then
-      echo "❌ Application is not accessible after $max_tries tries. Exiting..."
-      exit 1
-    fi
-
-    echo "Retrying in 10 seconds..."
-    sleep 10
-  fi
-done
+# Check if Frontend service is accessible
+echo -e "\e[33mWaiting\e[0m for frontend..."
+check_url "http://localhost:4200" 10 10
