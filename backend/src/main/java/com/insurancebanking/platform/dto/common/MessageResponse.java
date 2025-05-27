ff1b26d6411d6ef093032ae92bd427071d6d410a
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-27 18:19:11 +0200

    backend: Comments typo

--- backend/src/main/java/com/insurancebanking/platform/dto/common/MessageResponse.java
+++ backend/src/main/java/com/insurancebanking/platform/dto/common/MessageResponse.java
@@ -1,18 +1,23 @@
 package com.insurancebanking.platform.dto.common;
 
 public class MessageResponse {
+
     // Constructors
+
     public MessageResponse() {}
     public MessageResponse(String message) {
         this.message = message;
     }
 
-    // Fields
+    // Properties
+
     private String message;
 
     // Getters
+
     public String getMessage() { return message; }
 
     // Setters
+
     public void setMessage(String message) { this.message = message; }
 }
