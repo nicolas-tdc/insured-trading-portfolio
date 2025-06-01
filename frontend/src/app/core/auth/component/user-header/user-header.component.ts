036f5084e7f2a5193745c9416f91548d1c9aca95
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-01 19:00:40 +0200

    feature-auth, user-header: fix unused RouterLink warning

--- frontend/src/app/core/auth/component/user-header/user-header.component.ts
+++ frontend/src/app/core/auth/component/user-header/user-header.component.ts
@@ -9,7 +9,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
     BtnLogoutAuthComponent,
   ],
   templateUrl: './user-header.component.html',
