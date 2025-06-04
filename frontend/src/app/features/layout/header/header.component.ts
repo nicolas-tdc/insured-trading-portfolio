09ebd46df47fd5234026b0544b79206e4c1f40a7
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 13:04:33 +0200

    feature-transfer.frontend: Layout header uses mat-toolbar

--- frontend/src/app/features/layout/header/header.component.ts
+++ frontend/src/app/features/layout/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserHeaderComponent } from '../../../core/auth/component/user-header/user-header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
     UserHeaderComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
