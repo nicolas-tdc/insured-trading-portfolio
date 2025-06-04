92176baca7f4c6d390200f7dba2255b597156537
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 05:31:43 +0200

    feature-account.frontend: Template uses mat-card layout

--- frontend/src/app/core/account/component/account-details/account-details.component.ts
+++ frontend/src/app/core/account/component/account-details/account-details.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Account } from '../../model';
+import { MatCardModule } from '@angular/material/card';
+import { MatChip, MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-account-details',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+  ],
   templateUrl: './account-details.component.html',
   styleUrl: './account-details.component.scss'
 })
 export class AccountDetailsComponent {
 
-  // Properties, Accessors
+  // Properties
 
-  // Account
-  @Input() account: Account | null = null;
+  account = input<Account | null>();
 }
