050b58e363bdb666bb0fe39671dc26efe9f00933
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 05:43:59 +0200

    feature-policy.frontend: Use mat-card for policy details

--- frontend/src/app/core/policy/component/policy-details/policy-details.component.ts
+++ frontend/src/app/core/policy/component/policy-details/policy-details.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { Policy } from '../../model';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-policy-details',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+  ],
   templateUrl: './policy-details.component.html',
   styleUrl: './policy-details.component.scss'
 })
 export class PolicyDetailsComponent {
 
-  // Properties, Accessors
+  // Properties
 
-  // Policy
-  @Input() policy: Policy | null = null;
+  policy = input<Policy | null>();
 }
