6b51c57ca9dbafa5fb7f10a436cf45f83b4a11eb
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 05:45:13 +0200

    feature-policy.frontend: Policy services stores policies or policy in resource/signal

--- frontend/src/app/core/policy/policy.service.ts
+++ frontend/src/app/core/policy/policy.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, resource, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 
 import { Policy, PolicyRequest } from './model';
 
@@ -9,8 +9,47 @@ export class PolicyService {
 
   // Properties
 
-  // API
   private apiUrl = '/api/policy';
+
+  // Reactive list
+  private userPoliciesResource = this.createUserPoliciesResource();
+  public userPolicies = computed(() => this.userPoliciesResource.value());
+
+  public reloadUserPolicies(): void {
+    this.userPoliciesResource.reload();
+  }
+
+  private createUserPoliciesResource(): any {
+    return resource({
+      request: () => ({ }),
+      loader: async ({ }) => {
+        return await firstValueFrom(this.getUserList());
+      },
+    });
+  }
+
+  // Reactive item selected by ID
+  private selectedPolicyId = signal<string | null>(null);
+
+  public selectPolicy(id: string | null): void {
+    this.selectedPolicyId.set(id);
+  }
+
+  private userPolicyResource = this.createPolicyResource();
+  public userPolicy = computed(() => this.userPolicyResource.value());
+
+  createPolicyResource(): any {
+    return resource({
+      request: () => ({ accountId: this.selectedPolicyId() }),
+      loader: async ({ request }) => {
+        if (!request.accountId) return Promise.resolve(null);
+
+        return await firstValueFrom(
+          this.getItem(request.accountId)
+        );
+      }
+    })
+  }
 
   // Lifecycle
 
@@ -28,7 +67,7 @@ export class PolicyService {
     return this.http.get<Policy>(`${this.apiUrl}/${policyId}`);
   }
 
-  getList(): Observable<Policy[]> {
+  getUserList(): Observable<Policy[]> {
     return this.http.get<Policy[]>(`${this.apiUrl}`);
   }
 
