d470efe88f1c0c38fb18687a7f0083ac3e2546be
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 05:44:43 +0200

    feature-policy.frontend: Policy status in policy model

--- frontend/src/app/core/policy/model/policy.model.ts
+++ frontend/src/app/core/policy/model/policy.model.ts
@@ -1,10 +1,10 @@
 
 export interface Policy {
   id: string;
+  policyStatus: string;
   accountNumber: string;
   policyNumber: string;
   policyType: string;
   premium: number;
   coverageAmount: number;
-  status: string;
 }
