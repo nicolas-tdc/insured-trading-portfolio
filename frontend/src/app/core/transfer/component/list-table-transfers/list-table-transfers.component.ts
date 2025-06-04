8a49d07274eb8e237fdf58088da711e358337236
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 05:48:37 +0200

    feature-transfer.frontend: list-table-transfers uses mat-table

--- frontend/src/app/core/transfer/component/list-table-transfers/list-table-transfers.component.ts
+++ frontend/src/app/core/transfer/component/list-table-transfers/list-table-transfers.component.ts
@@ -1,25 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { ItemTableTransferComponent } from '../item-table-transfer/item-table-transfer.component';
-import { Transfer } from '../../model';
+import { Component, computed, inject, input, OnInit, resource } from '@angular/core';
+import { TransferService } from '../../transfer.service';
+import { MatTableModule } from '@angular/material/table';
+import { OtherAccountNumberPipe } from '../../pipe/other-account-number.pipe';
+import { DirectionalAmountPipe } from '../../pipe/directional-amount.pipe';
+import { AccountService } from '../../../account/account.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-table-transfers',
   imports: [
     CommonModule,
-    ItemTableTransferComponent
+    MatTableModule,
+    OtherAccountNumberPipe,
+    DirectionalAmountPipe,
   ],
   templateUrl: './list-table-transfers.component.html',
   styleUrl: './list-table-transfers.component.scss'
 })
 export class ListTableTransfersComponent {
 
-  // Properties, Accessors
+  // Properties
 
-  // Account Id
-  @Input() accountId: string = '';
+  public get account() {
+    return this.accountService.userAccount();
+  }
 
-  // Transfers
-  private _transfers: Transfer[] = [];
-  get transfers() { return this._transfers; }
+  public get transfers() {
+    return this.transferService.accountTransfers();
+  }
+
+  public displayedColumns: string[] = [
+    'date',
+    'amount',
+    'otherAccountNumber',
+    'description',
+  ];
+
+  // Lifecycle
+
+  constructor(
+    private transferService: TransferService,
+    private accountService: AccountService,
+    private route: ActivatedRoute,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.accountService.selectAccount(
+      this.route.snapshot.paramMap.get('accountId')!
+    );
+  }
 }
