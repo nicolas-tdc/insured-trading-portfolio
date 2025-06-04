36e037ec3d37ee3e62a3881d5558c967d698fb43
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-04 13:03:17 +0200

    feature-transfer.frontend: use mat-dialog and mat-button for accout page, open account form button

--- frontend/src/app/features/account-page/account-page.component.ts
+++ frontend/src/app/features/account-page/account-page.component.ts
@@ -1,44 +1,69 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Account } from '../../core/account/model';
+import { MatDialog } from '@angular/material/dialog';
 import { AccountService } from '../../core/account/account.service';
 import { AccountDetailsComponent } from '../../core/account/component/account-details/account-details.component';
 import { FormRequestTransferComponent } from '../../core/transfer/component/form-request-transfer/form-request-transfer.component';
+import { MatButton } from '@angular/material/button';
+import { ListTableTransfersComponent } from '../../core/transfer/component/list-table-transfers/list-table-transfers.component';
+import { TransferService } from '../../core/transfer/transfer.service';
+
 @Component({
   selector: 'app-account-page',
+  standalone: true,
   imports: [
     CommonModule,
     RouterLink,
+    MatButton,
     AccountDetailsComponent,
-    FormRequestTransferComponent,
+    ListTableTransfersComponent,
   ],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.scss'
 })
-export class AccountPageComponent {
+export class AccountPageComponent implements OnInit {
 
   // Properties
 
-  account = signal<Account | null>(null);
+  public get account() { return this.accountService.userAccount(); }
 
   // Lifecycle
 
   constructor(
-    private route: ActivatedRoute,
     private accountService: AccountService,
-  ) {}
+    private transferService: TransferService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
-    this.loadAccount();
+    const accountId = this.route.snapshot.paramMap.get('accountId');
+    if (!accountId) return;
+
+    this.accountService.selectAccount(accountId);
+    this.transferService.selectAccount(accountId);
   }
 
-  loadAccount(): void {
-    const paramId = this.route.snapshot.paramMap.get('accountId');
-    if (!paramId) return;
+  ngOnDestroy(): void {
+    this.accountService.selectAccount(null);
+    this.transferService.selectAccount(null);
+  }
+  // Transfer Dialog Form
 
-    this.accountService.getItem(paramId).subscribe(data => {
-      this.account.set(data);
+  openCreateTransferFormDialog(): void {
+    const accountValue = this.account;
+    if (!accountValue) return;
+
+    const dialogRef = this.dialog.open(FormRequestTransferComponent, {
+      width: '400px',
+      data: accountValue
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'completed') {
+        this.accountService.reloadUserAccount();
+      }
     });
   }
 }
