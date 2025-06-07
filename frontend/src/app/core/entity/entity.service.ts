91778786816f0a8e724fb5d5d6f59418898bdc9a
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-07 20:21:45 +0200

    feature-entity.frontend: Fix entity service's calls to account and policy services after split.

--- frontend/src/app/core/entity/entity.service.ts
+++ frontend/src/app/core/entity/entity.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { PolicyService } from '../policy/service/policy.service';
 import { TransferService } from '../transfer/transfer.service';
-import { CurrencyService } from '../currency/currency.service';
 import { AccountService } from '../account/service/account.service';
+import { UserAccountsService } from '../account/service/user-accounts.service';
+import { UserPoliciesService } from '../policy/service/user-policies.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,10 @@ export class EntityService {
 
   constructor(
     private accountService: AccountService,
+    private userAccountsService: UserAccountsService,
     private transferService: TransferService,
     private policyService: PolicyService,
+    private userPoliciesService: UserPoliciesService,
     private authService: AuthService,
   ) { }
 
@@ -22,11 +25,11 @@ export class EntityService {
     this.authService.clearAuthUser();
 
     // Accounts
-    this.accountService.clearUserAccounts();
+    this.userAccountsService.clearUserAccounts();
     this.accountService.clearSelectedAccount();
 
     // Policies
-    this.policyService.clearUserPolicies();
+    this.userPoliciesService.clearUserPolicies();
     this.policyService.clearSelectedPolicy();
 
     // Transfers
