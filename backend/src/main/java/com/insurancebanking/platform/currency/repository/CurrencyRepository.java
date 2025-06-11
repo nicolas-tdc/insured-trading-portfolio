2984504abd44b948d1c3d4b4246f9e28ebd09bdb
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-11 09:51:00 +0200

    feature-currency.backend: Remove unused currency model and repository, to fit java utils Currency

--- backend/src/main/java/com/insurancebanking/platform/currency/repository/CurrencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insurancebanking.platform.currency.repository;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.insurancebanking.platform.currency.model.Currency;
-
-public interface CurrencyRepository extends JpaRepository<Currency, UUID> { }
