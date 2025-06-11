08f5980d655416bfd327b9ddec442475f5f62ced
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-11 09:47:36 +0200

    feature-auth.backend: Fix missing service annotations

--- backend/src/main/java/com/insurancebanking/platform/auth/service/RoleService.java
+++ backend/src/main/java/com/insurancebanking/platform/auth/service/RoleService.java
@@ -1,10 +1,12 @@
 package com.insurancebanking.platform.auth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.insurancebanking.platform.auth.model.Role;
 import com.insurancebanking.platform.auth.repository.RoleRepository;
 
+@Service
 public class RoleService {
     
     @Autowired
--- backend/src/main/java/com/insurancebanking/platform/auth/service/UserService.java
+++ backend/src/main/java/com/insurancebanking/platform/auth/service/UserService.java
@@ -1,10 +1,12 @@
 package com.insurancebanking.platform.auth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.insurancebanking.platform.auth.model.User;
 import com.insurancebanking.platform.auth.repository.UserRepository;
 
+@Service
 public class UserService {
 
     @Autowired
