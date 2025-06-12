33a056f113687402d426568e39d9892fc5a563c5
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-12 14:26:03 +0200

    feature-policy.frontend: Policy components layout and use of copy to clipboard component

--- frontend/src/app/core/policy/component/policy-details/policy-details.component.ts
+++ frontend/src/app/core/policy/component/policy-details/policy-details.component.ts
@@ -3,6 +3,8 @@ import { Policy } from '../../model';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
+import { CopyToClipboardComponent } from '../../../utils/component/copy-to-clipboard/copy-to-clipboard.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-policy-details',
@@ -10,6 +12,8 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     MatCardModule,
     MatChipsModule,
+    MatButtonModule,
+    CopyToClipboardComponent,
   ],
   templateUrl: './policy-details.component.html',
   styleUrl: './policy-details.component.scss'
