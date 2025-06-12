26de2f60ceff4c9171e65f94a86fd38c5e7d788c
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-12 12:55:17 +0200

    feature-account.frontend: Account details display, layout and use copy to clipboard component

--- frontend/src/app/core/account/component/account-details/account-details.component.ts
+++ frontend/src/app/core/account/component/account-details/account-details.component.ts
@@ -3,6 +3,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
 import { Account } from '../../model';
+import { CopyToClipboardComponent } from '../../../utils/component/copy-to-clipboard/copy-to-clipboard.component';
+import { TransferService } from '../../../transfer/transfer.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-account-details',
@@ -10,6 +13,8 @@ import { Account } from '../../model';
     CommonModule,
     MatCardModule,
     MatChipsModule,
+    MatButton,
+    CopyToClipboardComponent,
   ],
   templateUrl: './account-details.component.html',
   styleUrl: './account-details.component.scss'
@@ -19,4 +24,16 @@ export class AccountDetailsComponent {
   // Properties
 
   account = input<Account | null>();
+
+  // Lifecycle
+
+  constructor(
+    private transferService: TransferService,
+  ) { }
+
+  // Dialog
+
+  openTransferDialog(): void {
+    this.transferService.openCreateTransferFormDialog(this.account());
+  }
 }
