4f8ed4d1d6214f2e6231ef3f46a9b6f31c4385e6
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-06-14 17:23:02 +0200

    feature-currency.backend: Currency response as record.

--- backend/src/main/java/com/insurancebanking/platform/currency/dto/CurrencyResponse.java
+++ backend/src/main/java/com/insurancebanking/platform/currency/dto/CurrencyResponse.java
@@ -2,40 +2,18 @@ package com.insurancebanking.platform.currency.dto;
 
 import java.util.Currency;
 
-public class CurrencyResponse {
-
-    // Constructors
-
-    public CurrencyResponse() {}
-
-    // Properties
-
-    private String currencyCode;
-    private String currencySymbol;
-    private int currencyFractionDigits;
-
-    // Response static builder
-
+public record CurrencyResponse(
+    String currencyCode,
+    String currencySymbol,
+    int currencyFractionDigits
+) {
     public static CurrencyResponse from(String currencyCode) {
-        CurrencyResponse response = new CurrencyResponse();
-
-        response.setCurrencyCode(currencyCode);
         Currency currency = Currency.getInstance(currencyCode);
-        response.setCurrencySymbol(currency.getSymbol());
-        response.setCurrencyFractionDigits(currency.getDefaultFractionDigits());
 
-        return response;
+        return new CurrencyResponse(
+            currencyCode,
+            currency.getSymbol(),
+            currency.getDefaultFractionDigits()
+        );
     }
-
-    // Getters
-
-    public String getCurrencyCode() { return currencyCode; }
-    public String getCurrencySymbol() { return currencySymbol; }
-    public int getCurrencyFractionDigits() { return currencyFractionDigits; }
-
-    // Setters
-
-    public void setCurrencyCode(String value) { this.currencyCode = value; }
-    public void setCurrencySymbol(String value) { this.currencySymbol = value; }
-    public void setCurrencyFractionDigits(int value) { this.currencyFractionDigits = value; }
 }
