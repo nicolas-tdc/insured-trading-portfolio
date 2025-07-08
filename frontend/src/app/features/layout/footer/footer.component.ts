89067a7b0beb18c071aeb6bb2d07775d1cbfbeb5
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-07-09 00:29:13 +0200

    feature-layout.frontend: Implement 3-sections footer: contacts & links, about me, portfolio tech stack.

--- frontend/src/app/features/layout/footer/footer.component.ts
+++ frontend/src/app/features/layout/footer/footer.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
