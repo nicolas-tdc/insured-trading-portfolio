a1d17d013ad59724e0bdcc00a4bff76e424f99bd
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-07-09 00:18:03 +0200

    feature-shared.frontend: Add assets folder to angular.json. Register icons at app init.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SvgIconService } from './core/shared/service/svg-icon.service';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'frontend';
 
-  constructor() { }
+  constructor(
+    private svgIconService: SvgIconService,
+  ) {
+    this.svgIconService.registerIcons();
+  }
 }
