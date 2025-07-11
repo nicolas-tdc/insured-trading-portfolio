b5a47eb325d41c0305bea38438ab990390170e02
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-07-11 21:37:15 +0200

    feature-authentication.frontend: Documentation, indentation.

--- frontend/src/app/features/authentication/authentication.component.ts
+++ frontend/src/app/features/authentication/authentication.component.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { FormLoginAuthComponent } from '../../core/auth/component/form-login-auth/form-login-auth.component';
 import { FormRegisterAuthComponent } from '../../core/auth/component/form-register-auth/form-register-auth.component';
 
+/**
+ * Authentication component
+ * 
+ * Displays the login and register forms for authentication.
+ */
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
@@ -15,6 +20,4 @@ import { FormRegisterAuthComponent } from '../../core/auth/component/form-regist
     HttpClient
   ],
 })
-export class AuthenticationComponent {
-
-}
+export class AuthenticationComponent { }
