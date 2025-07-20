974f1bdda086c9c7d501669c858ee33e3ce990d6
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-07-20 18:51:42 +0200

    feature-portfolio.frontend: Add close button to porftfolio infos dialog component.

--- frontend/src/app/core/portfolio/component/portfolio-infos-dialog/portfolio-infos-dialog.component.ts
+++ frontend/src/app/core/portfolio/component/portfolio-infos-dialog/portfolio-infos-dialog.component.ts
@@ -4,8 +4,15 @@ import { ContactLinksComponent } from '../contact-links/contact-links.component'
 import { TechStackComponent } from '../tech-stack/tech-stack.component';
 import { AboutThisComponent } from '../about-this/about-this.component';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
+/**
+ * Component for the portfolio infos dialog
+ * 
+ * @export
+ */
 @Component({
   selector: 'app-portfolio-infos-dialog',
   imports: [
@@ -14,11 +21,28 @@ import { MatCardModule } from '@angular/material/card';
     AboutThisComponent,
     TechStackComponent,
     MatDividerModule,
-    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './portfolio-infos-dialog.component.html',
   styleUrl: './portfolio-infos-dialog.component.scss'
 })
 export class PortfolioInfosDialogComponent {
 
+  /**
+   * Initializes the component
+   * Injects required services for dialog
+   * 
+   * @param dialogRef Service for dialog
+   */
+  constructor(
+    private readonly dialogRef: MatDialogRef<PortfolioInfosDialogComponent>,
+  ) { }
+
+  /**
+   * Closes the dialog
+   */
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
